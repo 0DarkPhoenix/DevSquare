44c623165a0910c3a1c5c223ad463295472060af
Dark_Phoenix_ <92178883+0DarkPhoenix@users.noreply.github.com>
2026-09-17 16:13:41 +0200

    fix: fixed SQL errors caused by incorrect syntax and reserved keywords

--- devsquare-cli/Scripts/init-tables.sql
+++ devsquare-cli/Scripts/init-tables.sql
@@ -12,91 +12,92 @@ END;
 $$ language 'plpgsql';
 
 /* USER TABLE */
-CREATE TABLE `user` (
-    id UUID PRIMARY KEY NOT NULL DEFAULT uuid7(),
+CREATE TABLE IF NOT EXISTS account (
+    id UUID PRIMARY KEY NOT NULL DEFAULT uuidv7(),
     username VARCHAR(40) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
-    blocked BOOLEAN NOT NULL DEFAULT false
-    deleted BOOLEAN NOT NULL DEFAULT false
-)
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    is_blocked BOOLEAN NOT NULL DEFAULT false,
+    is_deleted BOOLEAN NOT NULL DEFAULT false
+);
 
-CREATE TRIGGER set_updated_at
-BEFORE UPDATE ON `user`
+CREATE OR REPLACE TRIGGER set_updated_at 
+BEFORE UPDATE ON account
 FOR EACH ROW
-EXECUTE PROCEDURE update_timestamp();
+EXECUTE PROCEDURE update_timestamp_updated_at();
 
 
 /* ARTICLE TABLE */
-CREATE TABLE article (
+CREATE TABLE IF NOT EXISTS article (
     id BIGSERIAL PRIMARY KEY NOT NULL,
-    user_id UUID NOT NULL REFERENCES `user`(id),
+    account_id UUID NOT NULL REFERENCES account(id),
     title VARCHAR(50) NOT NULL,
     short_description VARCHAR(75) NOT NULL,
     code_blocks TEXT NOT NULL,
     body varchar(2000) NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-    draft boolean NOT NULL DEFAULT true,
-    published BOOLEAN NOT NULL DEFAULT false,
-    blocked BOOLEAN NOT NULL DEFAULT false,
+    is_draft boolean NOT NULL DEFAULT true,
+    is_published BOOLEAN NOT NULL DEFAULT false,
+    is_blocked BOOLEAN NOT NULL DEFAULT false,
     blocked_reason VARCHAR(255) DEFAULT NULL,
-    CHECK (NOT (draft AND published))
-)
+    CHECK (NOT (is_draft AND is_published))
+);
 
-CREATE INDEX idx_article_user_id
-    ON article (user_id);
+CREATE INDEX IF NOT EXISTS idx_article_user_id
+    ON article(account_id);
 CREATE TRIGGER set_updated_at
 BEFORE UPDATE ON article
 FOR EACH ROW
-EXECUTE PROCEDURE update_timestamp();
+EXECUTE PROCEDURE update_timestamp_updated_at();
 
 CREATE OR REPLACE FUNCTION force_unpublish_when_blocked()
 RETURNS TRIGGER AS $$
 BEGIN
-    IF NEW.blocked THEN
-        NEW.draft := false;
-        NEW.published := false;
+    IF NEW.is_blocked THEN
+        NEW.is_draft := false;
+        NEW.is_published := false;
     END IF;
 
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER set_blocked_status
+CREATE OR REPLACE TRIGGER set_blocked_status
 BEFORE INSERT OR UPDATE ON article
 FOR EACH ROW
 EXECUTE FUNCTION force_unpublish_when_blocked();
 
 
 /* TAG TABLE */
-CREATE TABLE tag (
+CREATE TABLE IF NOT EXISTS tag (
     name VARCHAR(40) PRIMARY KEY NOT NULL 
-)
+);
 
 /* ARTICLE_TAG TABLE */
-CREATE TABLE article_tag (
+CREATE TABLE IF NOT EXISTS article_tag (
     article_id BIGINT NOT NULL REFERENCES article(id),
     tag_name VARCHAR(20) NOT NULL REFERENCES tag(name),
     PRIMARY KEY (article_id, tag_name)
-)
+);
 
-CREATE INDEX idx_article_tag_tag_name_article_id
+CREATE INDEX IF NOT EXISTS idx_article_tag_tag_name_article_id
     ON article_tag (tag_name, article_id);
 
 /* LANGUAGE TABLE */
-CREATE TABLE language (
+CREATE TABLE IF NOT EXISTS language (
     name VARCHAR(40) PRIMARY KEY NOT NULL 
-)
+);
 
 /* ARTICLE_LANGUAGE TABLE */
-CREATE TABLE article_language (
+CREATE TABLE IF NOT EXISTS article_language (
     article_id BIGINT NOT NULL REFERENCES article(id),
     language_name VARCHAR(20) NOT NULL REFERENCES language(name),
     PRIMARY KEY (article_id, language_name)
-)
+);
 
 CREATE INDEX idx_article_language_language_name_article_id
     ON article_language (language_name, article_id);
+
